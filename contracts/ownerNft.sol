184790d77bb576f2196154047a801bc224182d97
totokazu <tkn.soccerwalker@gmail.com>
2022-10-12 17:38:22 +0900

    create new contract

---
+++ contracts/ownerNft.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+import "@oppenzepplin@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "hardhat/console.sol";
+
+contract ownerNft is ERC721 {
+	constructor(){
+		console.log("test");
+	}
+}
