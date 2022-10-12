1e30c57b18826dd10a11112d52910ef731151a81
happy-coke <www.keeen@gmail.com>
2022-10-12 18:11:11 +0900

    create safeMint method

--- contracts/ownerNft.sol
+++ contracts/ownerNft.sol
@@ -2,12 +2,22 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 import "hardhat/console.sol";
 
-contract ownerNft is ERC721 {
+contract ownerNft is ERC721, Ownable {
+
+	using Counters for Counters.Counter;
+	Counters.Counter private _tokenIds;
+
 	constructor() ERC721("ownerNft", "ONT") {
 		console.log("test");
 	}
 
-	
+	function safeMint(address _to) public {
+		_safeMint(_to, _tokenIds.current());
+		console.log("mint");
+		_tokenIds.increment();
+	}
 }
